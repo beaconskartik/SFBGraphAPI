bd826222ac13b74e764fc9245eba34b60ae6ada1
Kartik Sachan <you@example.com>
2019-03-19 22:18:07 +0530

    inital project setup

---
+++ GraphAPI/GraphAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GraphAPI
+//
+//  Created by Kartik Sachan on 19/03/19.
+//  Copyright © 2019 Microsoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
